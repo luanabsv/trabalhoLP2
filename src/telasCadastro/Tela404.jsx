e8e3007db603b001778c47ae18cf46ae7531bb69
Luana Beatriz Souza Venâncio <luanavenancio73643@gmail.com>
2023-10-08 22:16:20 -0300

    fix: ajustes textos

--- src/telasCadastro/Tela404.jsx
+++ src/telasCadastro/Tela404.jsx
@@ -7,7 +7,7 @@ export default function Tela404(props) {
             <Pagina>
                 <Alert variant="danger" style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%)', width: 'max-content'}}>
                     O sistema não oferece acesso a essa página.
-                    Utilize o Menu para acessar as opções válidas. Acesse a aba cadastros no menu para e selecione o tipo do cadastro desejado.
+                    Utilize o Menu para acessar as opções válidas. Acesse a aba cadastros no menu e selecione o tipo do cadastro desejado.
                 </Alert>
             </Pagina>
         </Container>
--- src/telasCadastro/TelaMenu.jsx
+++ src/telasCadastro/TelaMenu.jsx
@@ -6,7 +6,7 @@ export default function TelaMenu(props) {
     return (
         <Pagina>
             <Alert key={'warning'} variant={'warning'} style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%)', width: 'max-content'}}>
-                Acesse a aba cadastros no menu para e selecione o tipo do cadastro desejado
+                Acesse a aba cadastros no menu e selecione o tipo do cadastro desejado
             </Alert>
         </Pagina>
     );
