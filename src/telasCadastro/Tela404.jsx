505cc03e9f9221e86c95698de172c94357d1c255
Luana Beatriz Souza Venâncio <luanavenancio73643@gmail.com>
2023-10-08 22:06:22 -0300

    style: ajuste tela 404

--- src/telasCadastro/Tela404.jsx
+++ src/telasCadastro/Tela404.jsx
@@ -5,9 +5,9 @@ export default function Tela404(props) {
     return (
         <Container>
             <Pagina>
-                <Alert variant="danger">
+                <Alert variant="danger" style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%)', width: 'max-content'}}>
                     O sistema não oferece acesso a essa página.
-                    Utilize o Menu para acessar as opções válidas.
+                    Utilize o Menu para acessar as opções válidas. Acesse a aba cadastros no menu para e selecione o tipo do cadastro desejado.
                 </Alert>
             </Pagina>
         </Container>
